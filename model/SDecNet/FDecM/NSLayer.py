cc1e0c971141e29fa2c26d97a0483b9f2d6e6d7e
greekinRoma <zz8843157@gmail.com>
2025-10-21 01:01:57 +0800

    finish

--- model/SDecNet/FDecM/NSLayer.py
+++ model/SDecNet/FDecM/NSLayer.py
@@ -3,10 +3,8 @@ import torch
 class NSLayer(nn.Module):
     def __init__(self,channel,kernel=16):
         super(NSLayer, self).__init__()
-        self.basis_scale = torch.tensor([-4.9149e-01,  4.6062e-01,  7.8121e-01,  1.0924e+00,  1.5239e+00,
-        -2.2338e+00,  3.2156e+00, -3.6717e+00, -9.3707e+00,  4.7757e-03,
-        -2.6760e+01, -1.7231e+01,  5.6094e-03, -1.0633e-02]).view(14,1,1,1,1)
-        self.weight = nn.Parameter(self.basis_scale, requires_grad=False)
+        self.basis_scale = torch.randn(14,1,channel,1,1)
+        self.weight = nn.Parameter(self.basis_scale, requires_grad=True)
         self.mask = nn.Parameter(torch.eye(kernel).reshape(1,1,kernel,kernel),requires_grad=False)
     def forward(self, input):
         A = (self.mask - torch.matmul(input, torch.transpose(input, 2, 3)))
@@ -23,7 +21,7 @@ class NSLayer(nn.Module):
         L = torch.matmul(K, K)#**2048
         M = torch.matmul(L, L)#**4096
         N = torch.matmul(M, M)#**8192
-        weight = torch.abs(self.weight)
-        Mat =  weight[0]*A + weight[1] * B + weight[2]*C +weight[3]*D+  weight[4] * E+  weight[5] * F + weight[6] * G + weight[7] * H + weight[8] * I + weight[9] * J + weight[10] * K +weight[11] * L + weight[12] * M + weight[13] * N
+        weight = torch.relu(self.weight)
+        Mat =  weight[0]*A + weight[1] * B + weight[2]*C
         out = input + torch.matmul(Mat, input)
         return out
--- model/SDecNet/FDecM/SDecM.py
+++ model/SDecNet/FDecM/SDecM.py
@@ -3,7 +3,6 @@ from torch import nn
 import numpy as np
 from torch import nn
 import math
-from .NSLayer import NSLayer
 class SD2M(nn.Module):
     def __init__(self,in_channels,out_channels,shifts,kernel_size,use_norm=True):
         super().__init__()
@@ -23,36 +22,34 @@ class SD2M(nn.Module):
         kernel=np.concatenate([delta1,delta2],axis=0)
         self.kernel = torch.from_numpy(kernel).float().cuda()
         self.kernels = self.kernel.repeat(self.hidden_channels,1,1,1)
-        self.out_conv = nn.Sequential(nn.Conv2d(in_channels=self.hidden_channels,out_channels=self.in_channels,kernel_size=1,stride=1))
+        self.out_conv = nn.Sequential(nn.Conv2d(in_channels=self.hidden_channels,out_channels=self.hidden_channels,kernel_size=1,stride=1),
+                                      nn.Conv2d(in_channels=self.hidden_channels,out_channels=self.in_channels,kernel_size=1,stride=1))
         self.basis_convs = nn.ModuleList()
         self.origin_convs = nn.ModuleList()
         self.num_layer = 8
-        self.down_layer = nn.Conv2d(in_channels=self.in_channels,out_channels=self.hidden_channels,kernel_size=1,stride=1)
+        self.down_layer = nn.Sequential(nn.Conv2d(in_channels=self.in_channels,out_channels=self.hidden_channels,kernel_size=1,stride=1),
+                                        nn.BatchNorm2d(self.hidden_channels))
         self.origin_conv = nn.Sequential(
-            nn.Conv2d(in_channels=self.hidden_channels,out_channels=self.hidden_channels,kernel_size=1,stride=1),
+            nn.Conv2d(in_channels=self.hidden_channels,out_channels=self.hidden_channels,kernel_size=3,stride=1,padding=1),
+            nn.Conv2d(in_channels=self.hidden_channels,out_channels=self.hidden_channels,kernel_size=3,stride=1,padding=1),
             nn.Conv2d(in_channels=self.hidden_channels,out_channels=self.hidden_channels,kernel_size=1,stride=1),
         )
-        self.kernel = 4
-        self.NSs = NSLayer(kernel=self.kernel,channel=self.hidden_channels)
-        self.trans_layer = nn.Sequential(
-            nn.Conv2d(in_channels=self.hidden_channels,out_channels=self.hidden_channels*self.kernel,kernel_size=(self.num_layer*len(self.shifts),1)),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=self.hidden_channels*self.kernel,out_channels=self.hidden_channels*self.kernel,kernel_size=1),
-            )
-        self.max_pooling = nn.MaxPool2d(kernel_size=3,stride=1,padding=1)
-        self.avg_pooling = nn.AvgPool2d(kernel_size=3,stride=1,padding=1)
+        self.trans_conv = nn.Conv2d(in_channels=self.hidden_channels,out_channels=self.hidden_channels,kernel_size=1,stride=1)
+        self.max_pool = nn.MaxPool2d(kernel_size=3,stride=1,padding=1)
+        self.avg_pool = nn.AvgPool2d(kernel_size=3,stride=1,padding=1)
+        self.params = nn.Parameter(torch.ones(1,1,1,1),requires_grad=True).cuda()
     def Extract_layer(self,cen,b,w,h):
-        basises = []
+        basises = [(self.max_pool(cen)-self.avg_pool(cen)).view(b,self.hidden_channels,1,-1)]
         for i in range(len(self.shifts)):
-            basis = torch.nn.functional.conv2d(weight=self.kernels,stride=1,padding="same",input=cen,groups=self.hidden_channels,dilation=self.shifts[i]).view(b,self.hidden_channels,self.num_layer,-1)
+            basis = torch.nn.functional.conv2d(weight=self.kernels.to(cen.device),stride=1,padding="same",input=cen,groups=self.hidden_channels,dilation=self.shifts[i]).view(b,self.hidden_channels,self.num_layer,-1)
             basises.append(basis)
-        basis = torch.concat(basises,dim=2)
-        basis = self.trans_layer(basis).view(b,self.hidden_channels,self.kernel,-1)
-        basis = torch.nn.functional.normalize(basis,dim=-1,p=2)/math.sqrt(self.kernel)
-        basis2 = self.NSs(basis)
-        basis1 = basis2.transpose(-2,-1)
         origin = self.origin_conv(cen)
-        origin = origin.view(b,self.hidden_channels,1,-1)
+        # origin = origin + torch.rand_like(origin)
+        origin = self.trans_conv(origin)
+        origin=origin.view(b,self.hidden_channels,1,-1)
+        basis1 = torch.concat(basises,dim=2)
+        basis2 = torch.nn.functional.normalize(basis1,dim=-1)
+        basis1 = basis2.transpose(-2,-1)
         weight_score = torch.matmul(origin,basis1)
         out = torch.matmul(weight_score,basis2).view(b,self.hidden_channels,w,h)
         return out
