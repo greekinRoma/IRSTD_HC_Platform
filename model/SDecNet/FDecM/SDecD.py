ff5983c751bf7a8b04a0e63f605c192d32451ca7
greekinRoma <zz8843157@gmail.com>
2025-10-26 16:25:37 +0800

    finish

--- model/SDecNet/FDecM/SDecD.py
+++ model/SDecNet/FDecM/SDecD.py
@@ -1,7 +1,6 @@
 import numpy as np
 import torch
 from torch import nn
-from .SortConv import SortConv
 class SD2D(nn.Module):
     def __init__(self,dim,ratio=1):
         super().__init__()
@@ -28,8 +27,17 @@ class SD2D(nn.Module):
         self.trans_conv = nn.Conv2d(in_channels=dim,out_channels=dim,kernel_size=1,stride=1)
         self.params = nn.Parameter(torch.zeros(1,1,1,1),requires_grad=True).cuda()
     def Extract_layer(self,cen,b,w,h):
-        edge = torch.nn.functional.conv2d(weight=self.kernels.to(cen.device),stride=2,input=cen,groups=self.hidden_channels).view(b,self.hidden_channels,self.num_layer,h//2,w//2)
-        out = torch.mean(edge,dim=2)
+        edge = torch.nn.functional.conv2d(weight=self.kernels.to(cen.device),stride=2,input=cen,groups=self.hidden_channels).view(b,self.hidden_channels,self.num_layer,-1)
+        max1 = self.max_pool(cen)
+        max = max1.view(b,self.hidden_channels,1,-1)
+        basis = torch.concat([max,edge],dim=2)
+        Basis1 = torch.nn.functional.normalize(basis,dim=-1)
+        Basis2 = Basis1.transpose(-2,-1)
+        origins = self.origin_conv(cen)
+        origins = self.trans_conv(origins)
+        origins = origins.view(b,self.hidden_channels,1,-1)
+        weight_score = torch.matmul(origins,Basis2)
+        out = torch.matmul(weight_score,Basis1).view(b,self.hidden_channels,w//2,h//2)
         return out
     def forward(self,cen):
         b,_,w,h= cen.shape
--- model/SDecNet/FDecM/SDecM.py
+++ model/SDecNet/FDecM/SDecM.py
@@ -39,12 +39,19 @@ class SD2M(nn.Module):
         self.avg_pool = nn.AvgPool2d(kernel_size=3,stride=1,padding=1)
         self.params = nn.Parameter(torch.ones(1,1,1,1),requires_grad=True).cuda()
     def Extract_layer(self,cen,b,w,h):
-        basises = []
+        basises = [(self.max_pool(cen)-self.avg_pool(cen)).view(b,self.hidden_channels,1,-1)]
         for i in range(len(self.shifts)):
-            basis = torch.nn.functional.conv2d(weight=self.kernels.to(cen.device),stride=1,padding="same",input=cen,groups=self.hidden_channels,dilation=self.shifts[i]).view(b,self.hidden_channels,self.num_layer,h,w)
+            basis = torch.nn.functional.conv2d(weight=self.kernels.to(cen.device),stride=1,padding="same",input=cen,groups=self.hidden_channels,dilation=self.shifts[i]).view(b,self.hidden_channels,self.num_layer,-1)
             basises.append(basis)
+        origin = self.origin_conv(cen)
+        # origin = origin + torch.rand_like(origin)
+        origin = self.trans_conv(origin)
+        origin=origin.view(b,self.hidden_channels,1,-1)
         basis1 = torch.concat(basises,dim=2)
-        out = torch.mean(basis1,dim=2)
+        basis2 = torch.nn.functional.normalize(basis1,dim=-1)
+        basis1 = basis2.transpose(-2,-1)
+        weight_score = torch.matmul(origin,basis1)
+        out = torch.matmul(weight_score,basis2).view(b,self.hidden_channels,w,h)
         return out
     def forward(self,cen):
         b,_,w,h= cen.shape
