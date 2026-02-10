a1643d5ab8714ce448d267b22a27d118bd7d042d
greekinRoma <zz8843157@gmail.com>
2026-02-10 21:12:54 +0800

    update_files

--- update.sh
+++ update.sh
@@ -5,6 +5,7 @@ unset ftp_proxy
 unset rsync_proxy
 unset https_proxy
 git add *
+git restore *
 git commit -m update_files
 git push origin master
 echo "Files updated and pushed to remote repository."
